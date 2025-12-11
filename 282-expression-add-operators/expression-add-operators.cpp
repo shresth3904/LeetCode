fc688407508bd3001d932fd4a51942cae358312b
Shresth <74952411+shresth3904@users.noreply.github.com>
2025-12-12 00:35:43 +0530

    Time: 467 ms (8.63%) | Memory: 93.1 MB (52.50%) - LeetSync

---
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void gen(string num, int index, long long sum, long long prev, vector<string>& output, string curr_str, int target){
+        if (index == num.size()){
+            if (sum == target) {
+                output.push_back(curr_str);
+            }
+            return;
+        }
+        string operand = "";
+        int jump = 0;
+        for (int i = index; i < num.size(); i++){
+            if (i > index && num[index] == '0') break;
+            operand += num[i];
+            long long focus = stoll(operand);
+            jump++;
+            if (index == 0)
+                gen(num, i + 1, focus, focus, output, operand, target);
+            else{
+            
+        gen(num, i+1, sum+focus, focus, output, curr_str + '+' + operand, target);
+        gen(num, i+1, sum-focus, -focus, output, curr_str + '-' + operand, target);
+        //mul
+        long long res = (sum-prev) + (focus*prev);
+        gen(num, i+1, res, prev*focus, output, curr_str + '*' + operand, target);
+            }
+        }
+    }
+    vector<string> addOperators(string num, int target) {
+        vector<string> output;
+        string s = "";
+        if (num.size() == 1){
+            if (num[0]-'0' == target) {
+                s += num[0];
+                output.push_back(s);
+                return output;
+            }
+
+            else return {};
+        }
+        
+        
+        gen(num, 0, 0, 0, output, s, target);
+
+        return output;
+    }
+};
