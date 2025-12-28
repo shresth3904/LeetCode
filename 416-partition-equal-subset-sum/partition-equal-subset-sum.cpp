d4941b8ba8c88e667de4ea3ca47a48f706a2955e
Shresth <74952411+shresth3904@users.noreply.github.com>
2025-12-28 22:27:12 +0530

    Time: 162 ms (26.97%) | Memory: 17.5 MB (52.69%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    bool gen(vector<int>& nums, int index, int target, vector<vector<int>>& dp){
-        if (target == 0) return true;
-        if (index == 0){
-            return nums[index] == target;
-        } 
-        if (dp[index][target] != -1) return dp[index][target];
-        bool exc = gen(nums, index-1, target, dp);
-        bool inc = false;
-        if (target - nums[index] >= 0) inc = gen(nums, index-1, target - nums[index], dp);
-
-        return dp[index][target] = exc || inc;
-    }
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
-        int sum = 0;
-        for (int i: nums) sum += i;
-        if (sum%2 != 0) return false;
+        int sum = 0; for(int i: nums) sum += i;
+        if (sum %2 != 0) return false;
+        vector<vector<bool>> dp(n+1, vector<bool> (sum/2 + 1, 0));
+        for (int i = 0; i < n; i++) dp[i][0] = true;
+        if (nums[0] <= sum/2) dp[0][nums[0]] = true;
 
-        vector<vector<int>> dp(n+1, vector<int> (sum/2+1, -1));
-        return gen(nums, n-1, sum/2, dp);
+        for (int index = 1; index < n; index++){
+            for (int target = 0; target <= sum/2; target++){
+                bool exc = dp[index-1][target];
+                bool inc = false;
+                if (target - nums[index] >= 0) inc = dp[index-1][target-nums[index]];
+
+                dp[index][target] = inc || exc;
+            }
+        }
+
+        return dp[n-1][sum/2];
+        
     }
 };
