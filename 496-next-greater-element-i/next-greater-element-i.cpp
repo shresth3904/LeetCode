389c3aeed1ad4925e1269f893821a2047671e0a9
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-01-07 23:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (35.34%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int next(vector<int>& nums, int j){
-        int base = nums[j];
-        for (int i = j+1; i < nums.size(); i++){
-            if (nums[i] > base) return nums[i];
-        }
-
-        return -1;
-    }
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int, int> hash;
-        for (int i = 0; i < nums2.size(); i++){
-            hash[nums2[i]] = i;
+        stack<int> st;
+        int n = nums2.size()+1;
+        while (--n){
+            if (!st.empty()){
+                while (!st.empty() && st.top() < nums2[n-1]) st.pop();
+                hash[nums2[n-1]] = (!st.empty())? st.top():-1;
+            }
+            else hash[nums2[n-1]] = -1;
+
+            st.push(nums2[n-1]);
         }
 
-        for (int i = 0; i < nums1.size(); i++){
-            if (hash.find(nums1[i]) != hash.end()) nums1[i] = next(nums2, hash[nums1[i]]);
-            else nums1[i] = -1;
-        }
-
+        for (int i = 0; i < nums1.size(); i++) nums1[i] = hash[nums1[i]];
         return nums1;
     }
 };
