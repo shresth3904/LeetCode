2bfb3be2007fe484e17c0e4ad57331d3d3f78cfb
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-01-11 01:32:18 +0530

    Time: 49 ms (79.73%) | Memory: 33.2 MB (46.39%) - LeetSync

---
+++ 691-stickers-to-spell-word/stickers-to-spell-word.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string apply(string target,vector<int>& table){
+        string res = "";
+        vector<int> temp = table;
+
+        for (char c: target){
+            if (temp[c-'a'] > 0) temp[c-'a']--;
+            else res += c;
+        }
+        sort(res.begin(), res.end());
+        return res;
+    }
+    int gen(string target, vector<vector<int>>& table, unordered_map<string, int>& dp){
+        if (target == "") return 0;
+        if (dp.find(target) != dp.end()) return dp[target];
+        char focus = target[0];
+        int res = INT_MAX;
+        for (auto& i: table){
+            if (i[focus-'a'] > 0){
+                string new_target = apply(target, i);
+                int sub_res = gen(new_target, table, dp);
+                if (sub_res != -1) res = min(res, 1 + sub_res);
+            }
+        }
+
+        dp[target] = (res == INT_MAX)? -1: res;
+        return dp[target];
+
+    }
+    int minStickers(vector<string>& stickers, string target) {
+        vector<vector<int>> table;
+        for (string s: stickers){
+            vector< int> hash(26, 0);
+            for (char c: s) hash[c-'a']++;
+            table.push_back(hash);
+        }
+
+        unordered_map<string, int> dp;
+        return gen(target, table, dp);
+    }
+};
