720e8bbdb5e3e7e49c7ed262e998d55872e3b99d
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-01-11 19:17:01 +0530

    Time: 18 ms (74.94%) | Memory: 81.4 MB (58.55%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -2,37 +2,22 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> NLE(n, n);
         stack<int> st;
-        st.push(0);
-        for (int i = 1; i < n; i++){
-           while (!st.empty() && heights[st.top()] > heights[i]){
-                NLE[st.top()] = i;
-                st.pop();
-           }
-           st.push(i);
-        }
-
-        vector<int> PLE(n, -1);
-        stack<int> st2;
-        st2.push(n-1);
-        for (int i = n-2; i >= 0; i--){
-           while (!st2.empty() && heights[st2.top()] > heights[i]){
-                PLE[st2.top()] = i;
-                st2.pop();
-           }
-           st2.push(i);
-        }
-
-
         int max_area = 0;
+        for (int i = 0; i <= n; i++){
+            int h = (i == n)? 0: heights[i];
+            
+            while (!st.empty() && h < heights[st.top()]){
+                int temp = heights[st.top()];
+                st.pop();
 
-        for (int i = 0; i < n; i++){
-            max_area = max(max_area, heights[i]*(NLE[i]-PLE[i]-1));
+                int width = (st.empty())? i: i - st.top() -1;
+                max_area = max(max_area, temp*width); 
+            }
+
+            st.push(i);
         }
 
-        
         return max_area;
-
     }
 };
