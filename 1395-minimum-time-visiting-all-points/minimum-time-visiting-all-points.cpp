01b72e3290e6ca86ab64d6d9d5d09bbff110ecb0
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-01-12 23:14:14 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (61.26%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -7,7 +7,7 @@ public:
             int x = abs(points[i][0]-points[i-1][0]);
             int y = abs(points[i][1]-points[i-1][1]);
 
-            int time = min(x, y) + abs(x-y);
+            int time = max(x, y);
             total_time += time;
         }
 
