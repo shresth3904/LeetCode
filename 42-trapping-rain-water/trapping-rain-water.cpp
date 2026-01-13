a3f4058d0db33ff97b620ed38aaa9e27a03180c7
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-01-13 15:48:32 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (43.83%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -4,13 +4,13 @@ public:
         int n = height.size();
         vector<int> st;
         int res = 0;
-        for (int i = 0; i <= n; i++){
-            int bar = (i == n)? -1: height[i];
+        for (int i = 0; i < n; i++){
+            int bar = height[i];
             while (!st.empty() && height[st.back()] < bar){
                 int curr = st.back();
                 st.pop_back();
                 int PGE = (st.empty())? -1: st.back();
-                if (PGE != -1 && bar != -1) {
+                if (PGE != -1) {
                     res += (min(bar, height[PGE]) - height[curr]) * (i-PGE-1);
                 }
             }
