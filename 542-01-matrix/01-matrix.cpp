f0b5c630a93aa0a40f558fc46986b26e10a467cc
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-03-17 16:48:13 +0530

    Time: 171 ms (5.01%) | Memory: 74.8 MB (5.00%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -5,35 +5,34 @@ public:
         int n = mat[0].size();
 
         queue<pair<int, int>> q;
-
+        vector<vector<int>> res(m, vector<int>(n, -1));
         for (int i = 0; i < m; i++){
             for (int j = 0; j <n; j++){
-                if (mat[i][j] == 0) q.push({i, j});
+                if (mat[i][j] == 0) {
+                    q.push({i, j});
+                    res[i][j] = 0;
+                }
             }
         }
 
         vector<vector<int>> dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
-        vector<vector<int>> res(m, vector<int>(n, 1e9));
+        
         int lvl = 0;
         while (!q.empty()){
-            int k = q.size();
-            for (int i = 0; i < k; i++){
+            
                 auto top = q.front();
                 q.pop();
-
-                res[top.first][top.second] = min(res[top.first][top.second], lvl);
 
                 for (auto d: dir){
                     int sr = top.first + d[0];
                     int sc = top.second + d[1];
 
-                    if (sr >= 0 && sc >= 0 && sr < m && sc < n && mat[sr][sc] == 1 && res[sr][sc] == 1e9){
+                    if (sr >= 0 && sc >= 0 && sr < m && sc < n && mat[sr][sc] == 1 && res[sr][sc] == -1){
                         q.push({sr, sc});
+                        res[sr][sc] = 1 + res[top.first][top.second];
                     }
                 }
-            }
-            lvl++;
         }
 
         return res;
