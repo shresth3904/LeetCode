c8bd84db4eb828275ab407db6e07dd3afe695f8a
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-03-17 16:39:12 +0530

    Time: 393 ms (5.01%) | Memory: 118.9 MB (5.00%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -14,21 +14,21 @@ public:
 
         vector<vector<int>> dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
-        vector<vector<int>> res(m, vector<int>(n, -1));
+        vector<vector<int>> res(m, vector<int>(n, 1e9));
         int lvl = 0;
         while (!q.empty()){
             int k = q.size();
             for (int i = 0; i < k; i++){
                 auto top = q.front();
                 q.pop();
-                if (res[top.first][top.second] == -1) res[top.first][top.second] = lvl;
-                else res[top.first][top.second] = min(res[top.first][top.second], lvl);
+
+                res[top.first][top.second] = min(res[top.first][top.second], lvl);
 
                 for (auto d: dir){
                     int sr = top.first + d[0];
                     int sc = top.second + d[1];
 
-                    if (sr >= 0 && sc >= 0 && sr < m && sc < n && mat[sr][sc] == 1 && res[sr][sc] == -1){
+                    if (sr >= 0 && sc >= 0 && sr < m && sc < n && mat[sr][sc] == 1 && res[sr][sc] == 1e9){
                         q.push({sr, sc});
                     }
                 }
