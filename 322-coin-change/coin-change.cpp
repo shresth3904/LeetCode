cee76790bb377f87919d926155c181aac6433c50
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-03-27 10:34:44 +0530

    Time: 35 ms (50.75%) | Memory: 18.9 MB (58.03%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int gen(vector<int>& coins, int amt, vector<int>& dp){
+        if (amt == 0) return 0;
+        else if (amt < 0) return -1;
+
+        if (dp[amt] != -2) return dp[amt];
+
+        int n = coins.size();
+        int res = INT_MAX;
+        while (n--){
+            int count = gen(coins, amt - coins[n], dp);
+            if (count != -1) res = min(res, count +1); 
+        }
+        dp[amt] = (res == INT_MAX)? -1: res;
+        return dp[amt];
+
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, -2);
+
+        return gen(coins, amount, dp);
+    }
+};
