9e5b6237d9f469ae37ec6e916ca8cffc03693fdf
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-03-28 14:42:02 +0530

    Time: 13 ms (54.54%) | Memory: 25.8 MB (24.39%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    int gen(vector<int>& nums, int target, int index){
+    int gen(vector<int>& nums, int target, int index, vector<vector<int>>& dp, int& total_sum){
         if (index == nums.size()){
             if (target == 0) return 1;
             return 0;
         }
+        if (abs(target) > total_sum) return 0;
+        int offset_target = target + total_sum;
+        if (dp[index][offset_target] != -1) return dp[index][offset_target];
 
-        int count = gen(nums, target + nums[index], index + 1) + gen(nums, target - nums[index], index+1);
-        return count;
+        dp[index][offset_target] = gen(nums, target + nums[index], index + 1, dp, total_sum) + gen(nums, target - nums[index], index+1, dp, total_sum);
+        return dp[index][offset_target];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return gen(nums, target, 0);
+        int total_sum = 0; for (int i: nums) total_sum += i;
+        if (abs(target) > total_sum) return 0;
+        vector<vector<int>> dp(nums.size()+1, vector<int> (2* total_sum + 1, -1));
+        return gen(nums, target, 0, dp, total_sum);
     }
 };
