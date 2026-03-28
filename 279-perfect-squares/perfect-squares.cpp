ebd8805dcf36fa793b3bc74bb62b7ecc86491448
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-03-28 22:35:08 +0530

    Time: 39 ms (83.48%) | Memory: 13.2 MB (67.50%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int numSquares(int n) {
-        int lim = (int) pow(n, 0.5);
         vector<int> dp(n+1, n+1);
         dp[0] = 0;
-        for (int i = 1; i <= lim; i++){
+        for (int i = 1; i*i <= n; i++){
             for (int j = i*i; j <= n; j++){
                 dp[j] = min(dp[j], dp[j - i*i] + 1);
             }
