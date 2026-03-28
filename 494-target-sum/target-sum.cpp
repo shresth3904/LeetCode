3d58cf907c9608d32233c77665251e0483ce27b0
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-03-28 16:07:03 +0530

    Time: 4 ms (76.90%) | Memory: 12.2 MB (70.92%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int gen(vector<int>& nums, int target, int index, vector<vector<int>>& dp, int& total_sum){
-        if (index == nums.size()){
-            if (target == 0) return 1;
-            return 0;
-        }
-        if (abs(target) > total_sum) return 0;
-        int offset_target = target + total_sum;
-        if (dp[index][offset_target] != -1) return dp[index][offset_target];
-
-        dp[index][offset_target] = gen(nums, target + nums[index], index + 1, dp, total_sum) + gen(nums, target - nums[index], index+1, dp, total_sum);
-        return dp[index][offset_target];
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int total_sum = 0; for (int i: nums) total_sum += i;
-        if (abs(target) > total_sum) return 0;
-        vector<vector<int>> dp(nums.size()+1, vector<int> (2* total_sum + 1, -1));
-        return gen(nums, target, 0, dp, total_sum);
+        int total_sum = 0; for (int & i: nums) total_sum += i;
+        if ((total_sum+target) %2 != 0 || abs(target) > total_sum) return 0;
+
+        int target_sum = (total_sum+target)/2;
+        int n = nums.size();
+
+        vector<int> dp(target_sum + 1, 0);
+        dp[0] = 1;
+        
+        for (int num: nums){
+            for (int j = target_sum; j >= num; j--){
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[target_sum];
     }
 };
