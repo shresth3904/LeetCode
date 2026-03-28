efca2b1eef00353b8abf3d829d2fec83a07d017e
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-03-28 17:23:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (76.45%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int gen(int n, vector<int>& nums, vector<int>& dp){
-        if (n < 0) return 0;
-        if (dp[n] != -1) return dp[n];
-        dp[n] = nums[n] + max(gen(n-2, nums, dp), gen(n-3, nums, dp));
-        return dp[n];
-    }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n+1, -1);
-        return max(gen(n-1, nums, dp), gen(n-2, nums, dp));
+        int a = 0;
+        int b = 0;
+
+        for (int i = 0; i < nums.size(); i++){
+            int temp = a;
+            a = max(a, b+nums[i]);
+            b = temp;
+        }
+
+        return a;
     }
 };
