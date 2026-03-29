246e0af1b1e473ec2a11e27899f518c719959c55
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-03-29 18:24:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (74.31%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-    int gen(int m, int n, vector<vector<int>>& dp){
-        if (m == 1 && n == 1) return 1;
-        if (m == 0 || n == 0) return 0;
-        if (dp[m][n] != -1) return dp[m][n];
-        dp[m][n] = gen(m-1, n, dp) + gen(m, n-1, dp);
-        return dp[m][n];
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return gen(m, n, dp);
+        vector<int> dp(n, 0);
+        vector<int> dp2(n, 0);
+
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) dp[i] = dp[i-1];
+
+        for (int i = 1; i < m; i++){
+            dp2[0] = dp[0]; 
+            for (int j = 1; j < n; j++){
+                dp2[j] = dp2[j-1]+ dp[j];
+            }
+
+            dp.swap(dp2);
+        }
+
+        return dp[n-1];
     }
 };
