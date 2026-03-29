53ce73c5a10a27c4725aa920f057e6ebf538182c
Shresth <74952411+shresth3904@users.noreply.github.com>
2026-03-29 18:03:12 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (15.25%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int gen(vector<vector<int>>& grid, vector<vector<int>>&dp, int m, int n){
-        if (m == 1 && n == 1) return grid[0][0];
-        if (m == 0 || n == 0) return INT_MAX;
-
-        if (dp[m][n] != -1) return dp[m][n];
-
-        dp[m][n] = grid[m-1][n-1] + min(gen(grid, dp, m-1, n), gen(grid, dp, m, n-1));
-        return dp[m][n];
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
-        return gen(grid, dp, m, n);
+        vector<vector<int>> dp(m+1, vector<int> (n+1, 1e9));
+        dp[1][1] = grid[0][0];
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                dp[i+1][j+1] = min(dp[i+1][j+1], grid[i][j] + min(dp[i][j+1], dp[i+1][j]));
+            }
+        }
+
+        return dp[m][n];
     }
 };
